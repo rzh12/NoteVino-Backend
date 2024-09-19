b26a427afe6de8fa89f540efb2748a749103ba85
rzh12 <jrzhenhan@gmail.com>
2024-09-19 17:08:56 +0800

    [fix] Handle CORS issue in local React testing

---
+++ notevino/src/main/java/com/rzh12/notevino/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.rzh12.notevino.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 支持 OPTIONS 方法
+                .allowedHeaders("*")
+                .allowCredentials(true) // 根據需求允許憑證
+                .maxAge(3600); // 增加 maxAge 減少預檢請求
+    }
+}
